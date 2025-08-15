29b5996a99e8e367ac694a0ba52e4977276b2a04
Mehrab Mahmud Udoy <rimehrab@gmail.com>
2025-08-15 23:00:42 +0600

    add: ICC FTP BDIX Provider

---
+++ ICC FTP (BDIX)/build.gradle.kts
@@ -0,0 +1,40 @@
+// use an integer for version numbers
+version = 2
+
+android {
+    namespace = "com.rimehrab"
+}
+
+cloudstream {
+    // All of these properties are optional, you can safely remove them
+
+    description = "ICC FTP BDIX Provider"
+    authors = listOf("Mehrab Mahmud Udoy")
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+     * */
+    status = 1 // will be 3 if unspecified
+
+    // List of video source types. Users are able to filter for extensions in a given category.
+    // You can find a list of avaliable types here:
+    // https://recloudstream.github.io/cloudstream/html/app/com.lagradost.cloudstream3/-tv-type/index.html
+    tvTypes = listOf(
+        "Movie",
+        "TvSeries",
+        "Anime",
+        "AnimeMovie",
+        "OVA",
+        "Cartoon",
+        "AsianDrama",
+        "Others",
+        "Documentary",
+    )
+    language = "bn"
+
+    iconUrl = "https://raw.githubusercontent.com/rimehrab/extensions.cs3/master/ICC FTP (BDIX)/icon.png"
+}
