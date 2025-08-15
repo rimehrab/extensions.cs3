0148c20c0ecbcc07a78e5366a11a20c796f049f7
Mehrab Mahmud Udoy <rimehrab@gmail.com>
2025-08-15 23:01:19 +0600

    add: AnimeDekho Provider

---
+++ AnimeDekho/src/main/kotlin/com/rimehrab/AnimeDekhoPlugin.kt
@@ -0,0 +1,30 @@
+package com.rimehrab
+
+import com.lagradost.cloudstream3.plugins.BasePlugin
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.extractors.FileMoon
+import com.lagradost.cloudstream3.extractors.GDMirrorbot
+import com.lagradost.cloudstream3.extractors.Krakenfiles
+import com.lagradost.cloudstream3.extractors.StreamTape
+import com.lagradost.cloudstream3.extractors.Voe
+
+@CloudstreamPlugin
+class AnimeDekhoPlugin: BasePlugin() {
+    override fun load() {
+        registerMainAPI(AnimeDekhoProvider())
+        registerExtractorAPI(StreamRuby())
+        registerExtractorAPI(Vidmolynet())
+        registerExtractorAPI(GDMirrorbot())
+        registerExtractorAPI(Cdnwish())
+        registerExtractorAPI(Multimovies())
+        registerExtractorAPI(FileMoon())
+        registerExtractorAPI(FileMoonNL())
+        registerExtractorAPI(Krakenfiles())
+        registerExtractorAPI(Voe())
+        registerExtractorAPI(StreamTape())
+        registerExtractorAPI(FilemoonV2())
+        registerExtractorAPI(Animezia())
+        registerExtractorAPI(Cloudy())
+        registerExtractorAPI(vidcloudupns())
+    }
+}
